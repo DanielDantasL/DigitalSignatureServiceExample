f414ba7dc2b2711ed9cc4e9e2811dbd8ee82b8ee
Daniel Dantas <daniel.leite.dantas@gmail.com>
2021-06-19 17:41:05 -0300

    Asymmetric key generation UI
    Important: Blazor WebAssembly does not support System.Security.Cryptography: https://docs.microsoft.com/en-us/dotnet/core/compatibility/cryptography/5.0/cryptography-apis-not-supported-on-blazor-webassembly
    We need to change ASAP

--- DigitalSignatureService/Client/Program.cs
+++ DigitalSignatureService/Client/Program.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using BlazorDownloadFile;
 using MudBlazor.Services;
 
 namespace DigitalSignatureService.Client
@@ -21,6 +18,7 @@ namespace DigitalSignatureService.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMudServices();
+            builder.Services.AddBlazorDownloadFile(lifetime: ServiceLifetime.Scoped);
             
             await builder.Build().RunAsync();
         }
---
+++ DigitalSignatureService/Shared/CryptoUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text.Unicode;
+
+namespace DigitalSignatureService.Shared {
+    public static class CryptoUtils {
+        public enum KeySize {
+            Bits515  = 515,
+            Bits1024 = 1024,
+        }
+
+        public enum KeyGenAlgorithm {
+            RSA = 1,
+        }
+
+
+        public static bool TryGenerateKeyPair(KeyGenAlgorithm algorithm, KeySize keySize, out string publicKey, out string privateKey) {
+            switch (algorithm) {
+                case KeyGenAlgorithm.RSA: return TryGenerateKeyPairRsa(keySize, out publicKey, out privateKey);
+            }
+
+            publicKey  = null;
+            privateKey = null;
+            return false;
+        }
+
+        private static bool TryGenerateKeyPairRsa(KeySize keySize, out string publicKey, out string privateKey) {
+            try {
+                var result = RSA.Create((int) keySize);
+                publicKey  = System.Text.Encoding.Default.GetString(result.ExportRSAPublicKey());
+                privateKey = System.Text.Encoding.Default.GetString(result.ExportRSAPrivateKey());
+                return true;
+            } catch (Exception e) {
+                publicKey  = e.Message;
+                privateKey = e.Message;
+                return false;
+            }
+        }
+    }
+}
